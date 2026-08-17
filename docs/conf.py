6a7cd6bd15fc3f7e1024b406ce981f487b5d13b6
psulewski <psulewski@uos.de>
2026-08-17 08:45:52 +0200

    docs and requirements

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,8 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.githubpages',
     'myst_parser',
+    'sphinx_design',
+    'sphinx_copybutton',
 ]
 
 # Napoleon settings
@@ -70,39 +72,37 @@ source_suffix = {
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 html_static_path = ['_static']
 
 # Theme options
 html_theme_options = {
-    'canonical_url': '',
-    'analytics_id': '',
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': '#2980B9',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
-
-# Custom sidebar
-html_sidebars = {
-    '**': [
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-    ]
+    'sidebar_hide_name': False,
+    'footer_icons': [
+        {
+            'name': 'GitHub',
+            'url': 'https://github.com/KietzmannLab/pyavs',
+            'html': (
+                '<svg stroke="currentColor" fill="currentColor" stroke-width="0" '
+                'viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 '
+                '8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-'
+                '2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 '
+                '1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-'
+                '.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82'
+                '.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 '
+                '1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 '
+                '1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-'
+                '3.58-8-8-8z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>'
+            ),
+            'class': '',
+        },
+    ],
 }
 
 # HTML context
 html_context = {
     'display_github': True,
-    'github_user': 'your-org',
+    'github_user': 'KietzmannLab',
     'github_repo': 'pyavs',
     'github_version': 'main',
     'conf_py_path': '/docs/',
@@ -136,29 +136,12 @@ myst_enable_extensions = [
 ]
 
 # -- Mock imports for ReadTheDocs -------------------------------------------
+# matplotlib/PIL/h5py are genuinely installed via docs/requirements.txt, so they
+# are intentionally NOT mocked here (mocking a package that's actually available
+# would throw away real type hints/docstrings in the rendered API docs).
 autodoc_mock_imports = [
     'mne',
     'sklearn',
-    'matplotlib',
-    'PIL',
     'cv2',
     'pycocotools',
-    'h5py',
 ]
-
-# Mock modules that may not be available during doc build
-import unittest.mock as mock
-
-MOCK_MODULES = [
-    'mne', 'mne.io', 'mne.preprocessing', 'mne.minimum_norm', 'mne.beamformer',
-    'mne.transforms', 'mne.bem', 'mne.time_frequency', 'mne.io.constants',
-    'sklearn', 'sklearn.preprocessing', 'sklearn.decomposition',
-    'matplotlib', 'matplotlib.pyplot',
-    'PIL', 'PIL.Image',
-    'cv2',
-    'pycocotools', 'pycocotools.coco', 'pycocotools.mask',
-    'h5py',
-]
-
-# for mod_name in MOCK_MODULES:
-#     sys.modules[mod_name] = mock.MagicMock()
